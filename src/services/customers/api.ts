7d4bb1f5bdf4022e8f7e0883a1139878784c8f86
Falah Abdurrafi <Falah.Abdurrafi@erraipasifik.com>
2024-12-11 04:10:45 -0500

    Get Customers Data

---
+++ src/services/customers/api.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://northwind.falahrafi.com/api.php/records/customers';
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export const fetchCustomers = async () => {
+  const response = await api.get('/');
+  return response.data.records;
+};
+
+export const fetchCustomerById = async (id: number) => {
+  const response = await api.get(`/${id}`);
+  return response.data;
+};
+
+export const createCustomer = async (customer: Record<string, any>) => {
+  const response = await api.post('/', customer);
+  return response.data;
+};
+
+export const updateCustomer = async (id: number, customer: Record<string, any>) => {
+  const response = await api.put(`/${id}`, customer);
+  return response.data;
+};
+
+export const deleteCustomer = async (id: number) => {
+  const response = await api.delete(`/${id}`);
+  return response.data;
+};
